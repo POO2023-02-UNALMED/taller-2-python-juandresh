cf927a2e1d69600edbcaedf50d8294f6433bd765
juandresh <141792824+juandresh@users.noreply.github.com>
2023-08-19 23:22:00 -0500

    creación de métodos

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 class asiento:
-    def __init__(self, color, precio, registro)
+    def __init__(self, color, precio, registro):
         self.color=color
         self.precio=precio
         self.registro=registro
 
+    def cambiarColor(self, color):
+
 
 class auto:
-    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados)
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
         self.modelo=modelo
         self.precio=precio
         self.asientos=asientos
         self.marca=marca
         self.motor=motor
         self.registro=registro
-        self.cantidadCreados=cantidadCreados
+        cantidadCreados=cantidadCreados
+
+    def cantidadAsientos(self, asientos):
+
+    def verificarIntegridad(self, registro):
 
 
 class motor:
-    def __init__(self, numeroCilindros, tipo, registro)
+    def __init__(self, numeroCilindros, tipo, registro):
         self.numeroCilindros=numeroCilindros
         self.tipo=tipo
         self.registro=registro
+
+    def cambiarRegistro(self, registro):
+        
+    def asignarTipo(self, tipo):
