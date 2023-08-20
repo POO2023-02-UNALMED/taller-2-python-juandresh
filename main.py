e6cc69c0a2686d75e6fe1334e237ede489e5cb03
juandresh <141792824+juandresh@users.noreply.github.com>
2023-08-19 23:09:15 -0500

    creación de clases y metodos

---
+++ main.py
@@ -0,0 +1,23 @@
+class asiento:
+    def __init__(self, color, precio, registro)
+        self.color=color
+        self.precio=precio
+        self.registro=registro
+
+
+class auto:
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados)
+        self.modelo=modelo
+        self.precio=precio
+        self.asientos=asientos
+        self.marca=marca
+        self.motor=motor
+        self.registro=registro
+        self.cantidadCreados=cantidadCreados
+
+
+class motor:
+    def __init__(self, numeroCilindros, tipo, registro)
+        self.numeroCilindros=numeroCilindros
+        self.tipo=tipo
+        self.registro=registro
